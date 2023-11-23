5272c6de03d260b192f8d128a3b6028097377179
Châu Đăng Huy <chaudanghuy@gmail.com>
2023-11-23 21:56:26 +0700

    Photo screen

--- lib/screens/photo_cam.dart
+++ lib/screens/photo_cam.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+import 'package:photobooth_section1/models/image_model.dart';
+import 'package:photobooth_section1/screens/photo_list_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CameraApp extends StatefulWidget {
   @override
@@ -11,20 +17,22 @@ class _CameraAppState extends State<CameraApp> {
   late CameraController _controller;
   late List<CameraDescription> cameras;
   bool isCameraReady = false;
+  final List<String> images = [];
 
-  int _countdown = 5; // Set your desired countdown time
+  int _countdown = 5;
   late Timer _timer;
 
   @override
   void initState() {
     super.initState();
+    _loadStoredValue();
     _initializeCamera();
   }
 
   Future<void> _initializeCamera() async {
     cameras = await availableCameras();
 
-    _controller = CameraController(cameras[0], ResolutionPreset.medium);
+    _controller = CameraController(cameras[0], ResolutionPreset.ultraHigh);
     await _controller.initialize();
 
     if (!mounted) return;
@@ -57,8 +65,32 @@ class _CameraAppState extends State<CameraApp> {
         // Capture photo
         final XFile file = await _controller.takePicture();
 
-        // Do something with the captured photo file
-        print('Photo captured: ${file.path}');
+        // Get directory to save
+        Directory _documentDir = Directory('');
+        _documentDir = await getApplicationDocumentsDirectory();
+
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        String _username = prefs.getString('username') ?? "";
+
+        final String userDir = path.join(_documentDir.path, _username);
+        await Directory(userDir).create(recursive: true);
+
+        final String userPath = path.join(userDir, path.basename(file.path));
+        await File(file.path).copy(userPath);
+
+        // Test
+        print('Photo captured: $userPath');
+
+        images.add(userPath);
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PhotoListScreen(
+              images: images,
+            ),
+          ),
+        );
       } catch (e) {
         print('Error taking photo: $e');
       }
@@ -79,24 +111,44 @@ class _CameraAppState extends State<CameraApp> {
     }
 
     return Scaffold(
-      body: Stack(
-        children: [
-          CameraPreview(_controller),
-          Center(
-            child: Text(
-              '$_countdown',
-              style: TextStyle(fontSize: 40.0, color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
           startTimer();
         },
-        child: Icon(Icons.camera),
+        child: PhotoCamForm(_countdown, _controller),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+}
+
+class PhotoCamForm extends StatelessWidget {
+  PhotoCamForm(this._countdown, this._controller);
+
+  final int _countdown;
+  final CameraController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CameraPreview(_controller),
+        Center(
+          child: Text(
+            '$_countdown',
+            style: TextStyle(fontSize: 60.0, color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+_loadStoredValue() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  Directory _documentDir = Directory('');
+  _documentDir = await getApplicationDocumentsDirectory();
+  print('on home screen');
+  print(prefs.getString('username'));
+  print('document dir' + _documentDir.path);
 }
--- lib/screens/photo_list_screen.dart
+++ lib/screens/photo_list_screen.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:photobooth_section1/models/image_model.dart';
+import 'package:photobooth_section1/screens/photo_cam.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../widgets/nav-drawer.dart';
 import '../data_sources/helper.dart';
@@ -10,7 +14,8 @@ import '../data_sources/helper.dart';
 const _defaultColor = Color(0xFF34568B);
 
 class PhotoListScreen extends StatefulWidget {
-  const PhotoListScreen({super.key});
+  final List<String> images;
+  PhotoListScreen({super.key, required this.images});
 
   @override
   State<StatefulWidget> createState() => _PhotoListScreenState();
@@ -20,24 +25,10 @@ class _PhotoListScreenState extends State<PhotoListScreen> {
   // late Timer timer;
   // late int seconds;
   PhotoHelper photoHelper = new PhotoHelper();
+  List<String> images = [];
 
   @override
   void initState() {
-    /*
-    seconds = 0;
-    timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
-      setState(() {
-        seconds++;
-      });
-
-      if(seconds == 30) {
-        Navigator.pushNamedAndRemoveUntil(
-            context, '/home', ModalRoute.withName('/home'));
-      }
-      print('on time ');
-      print(seconds);
-    });
-     */
     photoHelper.expireScreen(context);
     super.initState();
   }
@@ -50,9 +41,12 @@ class _PhotoListScreenState extends State<PhotoListScreen> {
 
   int _selectedIndex = 1;
   static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('From galary', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('From camera', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Home Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('From galary',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('From camera',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
   ];
 
   void _onItemTapped(int index) {
@@ -60,89 +54,51 @@ class _PhotoListScreenState extends State<PhotoListScreen> {
     // if(index == 1) {
     //   _pickImagefromGallery();
     // }
-    if(index == 0) {
+    if (index == 0) {
       Navigator.pushNamedAndRemoveUntil(
           context, '/home', ModalRoute.withName('/home'));
     }
-    if(index == 2) {
-      Navigator.pushNamedAndRemoveUntil(
-          context, '/photo_cam', ModalRoute.withName('/photo_cam'));
+    if (index == 2) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => CameraApp()));
     }
 
     setState(() {
       _selectedIndex = index;
     });
   }
-/*
-  // Generate a list of dummy items
-  final List<Map<String, dynamic>> _items =
-    List.generate(
-      20, (index) => {
-        "id": index,
-        "title": "Item $index",
-        "height": Random().nextInt(350) + 150.5
-      });
-
-*/
-  final List<Map<String, dynamic>> _items = [
-    {"id": 0, "title": "Item 0", "actPage": '/home', "imgUrl": "assets/images/list-doc/anime.gif"},
-    {'id': 1, 'title': "Item 1", 'actPage': 'cate1', "imgUrl": 'assets/images/list-doc/caricature.gif'},
-    {'id': 2, 'title': "Item 2", 'actPage': 'cate2', "imgUrl": 'assets/images/list-doc/cartoon.gif'},
-    {'id': 3, 'title': "Item 3", 'actPage': 'cate3', "imgUrl": 'assets/images/list-doc/comic.gif'},
-    {'id': 4, 'title': "Item 4", 'actPage': 'cate4', "imgUrl": 'assets/images/list-doc/pixar.gif'},
-    {'id': 5, 'title': "Item 5", 'actPage': 'cate5', "imgUrl": 'assets/images/list-doc/slamdunk.gif'},
-    {'id': 6, 'title': "Item 6", 'actPage': 'cate6', "imgUrl": 'assets/images/list-doc/anime.gif'},
-    {'id': 7, 'title': "Item 7", 'actPage': 'cate7', "imgUrl": 'assets/images/list-doc/caricature.gif'},
-    {'id': 8, 'title': "Item 8", 'actPage': 'cate8', "imgUrl": 'assets/images/list-doc/slamdunk.gif'},
-  ];
-
 
   @override
   Widget build(BuildContext context) {
+    images = widget.images;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Photo Editor Section"),
         centerTitle: true,
       ),
-      body: MasonryGridView.count(
-          itemCount: _items.length,
-          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
-          // the number of columns
-          crossAxisCount: 2,
-          // vertical gap between two items
-          mainAxisSpacing: 4,
-          // horizontal gap between two items
-          crossAxisSpacing: 5,
-          itemBuilder: (context, index) {
-            // display each item with a card
-            return Card(
-              // Give each item a random background color
-              color: Color.fromARGB(
-                  Random().nextInt(256),
-                  Random().nextInt(256),
-                  Random().nextInt(256),
-                  Random().nextInt(256)),
-              key: ValueKey(_items[index]['id']),
-              child: SizedBox(
-                //height: _items[index]['height'],
-                child: InkWell(
-                  onTap:(){
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, _items[index]['actPage'], ModalRoute.withName(_items[index]['actPage']));
-                  },
-                  child: Column(
-                    children: [
-                      //Image.network(_items[index]['imgUrl']),
-                      Image.asset(_items[index]['imgUrl']),
-                      Text(_items[index]['title']),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Number of columns in the grid
+          crossAxisSpacing: 8.0, // Spacing between columns
+          mainAxisSpacing: 8.0, // Spacing between rows
         ),
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              print('Tapped on image at index $index');
+            },
+            child: Card(
+              elevation: 2.0,
+              child: Image.file(
+                File(images[index]),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
       drawer: const NavDrawer(),
       bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
@@ -166,10 +122,12 @@ class _PhotoListScreenState extends State<PhotoListScreen> {
           unselectedItemColor: Colors.grey,
           iconSize: 40,
           onTap: _onItemTapped,
-          elevation: 5
-      ),
+          elevation: 5),
     );
-
   }
+}
 
+_loadImageList() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getStringList('images') as List<String>;
 }
