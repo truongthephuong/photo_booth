e8ef5a230f6722459d1d1abaaf72009f680d948a
Châu Đăng Huy <chaudanghuy@gmail.com>
2023-12-03 08:54:02 +0700

    Template v2 (wip)

--- lib/screens/screen3.dart
+++ lib/screens/screen3.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_image/flutter_native_image.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:photobooth_section1/models/image_model.dart';
 import 'package:photobooth_section1/screens/screen4.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart' as path;
 
 class Screen3 extends StatefulWidget {
   @override
@@ -11,6 +19,7 @@ class _Screen3State extends State<Screen3> {
   late CameraController _controller;
   late List<CameraDescription> cameras;
   bool isCameraReady = false;
+  List<ImageModel> savedImages = [];
 
   @override
   void initState() {
@@ -35,6 +44,84 @@ class _Screen3State extends State<Screen3> {
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  void startTimer() {
+    takePhoto();
+  }
+
+  /**
+   * Create folder inside root directory
+   * 
+   * Snap photo -> go to 'myphotos/{userID}/temp'
+   * If user choose photo -> go into 'myphotos/{userID}/Target'
+   * If photo apply AI -> go to Result with format 'myphotos/{userID}/Result/{Effect-ID}.png'
+   */
+  void takePhoto() async {
+    if (_controller.value.isInitialized) {
+      try {
+        // Capture photo
+        final XFile file = await _controller.takePicture();
+
+        // Get directory to save
+        Directory _tempDir = Directory('');
+        _tempDir = await getTemporaryDirectory();
+
+        Directory current = Directory.current;
+
+        print('current dir');
+        print(current.path);
+
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+
+        // Parent folder
+        final String internalFolder = path.join(current.path, 'myphotos');
+        await Directory(internalFolder).create(recursive: true);
+
+        // User folder
+        DateTime now = DateTime.now();
+        String _username = DateFormat('yyyyMMddkk').format(now);
+        await prefs.setString("username", _username);
+
+        final String userDir = path.join(internalFolder, _username);
+        await Directory(userDir).create(recursive: true);
+
+        // Temp folder
+        final String tempUserDir = path.join(userDir, 'temp');
+        await Directory(tempUserDir).create(recursive: true);
+
+        // Save photo
+        final String userPath =
+            path.join(tempUserDir, path.basename(file.path));
+        await File(file.path).copy(userPath);
+
+        _resizePhoto(userPath);
+
+        final ImageModel savedImage = ImageModel(
+          id: savedImages.length + 1,
+          title: '${_username} ${savedImages.length + 1}',
+          actPage: 'actPaged1',
+          imgUrl: userPath,
+        );
+
+        setState(() {
+          savedImages.add(savedImage);
+        });
+      } catch (e) {
+        print("Error taking photo: $e");
+      }
+    }
+  }
+
+  Future<String> _resizePhoto(String filePath) async {
+    ImageProperties properties =
+        await FlutterNativeImage.getImageProperties(filePath);
+    int? width = properties.width;
+    File compressedFile = await FlutterNativeImage.compressImage(filePath,
+        quality: 80, targetWidth: 640, targetHeight: 628);
+    print('resize path');
+    print(compressedFile.path);
+    return compressedFile.path;
   }
 
   Widget build(BuildContext context) {
@@ -126,10 +213,7 @@ class _Screen3State extends State<Screen3> {
                             child: ElevatedButton(
                               onPressed: () {
                                 // Add
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Screen4()));
+                                startTimer();
                               },
                               child: Padding(
                                 padding: const EdgeInsets.symmetric(
